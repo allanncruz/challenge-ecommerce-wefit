4645596b5b8d97effd025a70a610e27805d6f763
Allan <contato@allancruz.com.br>
2024-03-19 10:07:28 -0300

    feat: responsive and containers

---
+++ src/pages/Cart/style.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100%;
+  padding: 0 16px;
+  margin: 24px auto;
+  max-width: 1140px;
+  background-color: #fff;
+  border-radius: 4px;
+`
--- src/pages/Home/style.ts
+++ src/pages/Home/style.ts
@@ -8,4 +8,12 @@ export const MainContainer = styled.div`
   display: grid;
   grid-template-columns: repeat(3, 1fr);
   gap: 5px;
+
+  @media (max-width: 1024px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+
+  @media (max-width: 768px) {
+    grid-template-columns: repeat(1, 1fr);
+  }
 `
